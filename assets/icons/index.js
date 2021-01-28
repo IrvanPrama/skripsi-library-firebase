5ec4d91a9acf273aae4b154cb4c25fb8e284dab0
Revan/Irvan/Erpan <47099428+IrvanPrama@users.noreply.github.com>
2021-01-29 04:03:20 +0800

    Add files via upload

---
+++ assets/icons/index.js
@@ -0,0 +1,11 @@
+import { IconHomeActive } from "./home.svg";
+import { IconHome } from "./unhome.svg";
+import { IconProfileActive } from "./profile.svg";
+import { IconProfile } from "./unprofile.svg";
+
+export {
+    IconHomeActive,
+    IconHome,
+    IconProfileActive,
+    IconProfile,
+}
